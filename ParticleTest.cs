c63c3984358e74f18d93bd2d248cc28c65769760
haeun0877 <70643208+haeun0877@users.noreply.github.com>
2020-12-02 23:34:03 +0900

    Create ParticleTest.cs

---
+++ ParticleTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Par : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         * ps.Play(); ->ps실행
+         * ps.Stop(); ->ps종료
+         * ps.Pause(); ->일시정지
+         * ps.Emit(1); -> 입자한개가 뿜어져나옴 (ps.Emit(100)->100개)
+         */
+    }
+}
