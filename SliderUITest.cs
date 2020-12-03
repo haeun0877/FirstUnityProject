ac2d748dd880d28563ccad1e850876f57528d376
haeun0877 <70643208+haeun0877@users.noreply.github.com>
2020-12-03 15:47:54 +0900

    Create SliderUITest.cs

---
+++ SliderUITest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderUITest : MonoBehaviour
+{
+
+    [SerializeField] private Slider slider;
+
+    private bool isClick = false;
+
+    private float dotTime = 1f;
+    private float currentDotTime = 0f;
+
+    void Start()
+    {
+        currentDotTime = dotTime;
+    }
+
+    private void Update()
+    {
+        if (isClick)
+            slider.value -= Time.deltaTime;
+    }
+
+    public void Button()
+    {
+        isClick = true;
+    }
+
+    
+}
