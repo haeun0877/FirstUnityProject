57444cf94d8801733e6152bbe0d74a4663b1f61b
haeun0877 <70643208+haeun0877@users.noreply.github.com>
2020-12-06 22:01:19 +0900

    Create ComunicationTest.cs

---
+++ ComunicationTest.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Dialogue
+{
+    [TextArea]
+    public string dialogue;
+    public Sprite cg;
+}
+
+public class ComunicationTest : MonoBehaviour
+{
+
+    [SerializeField] private SpriteRenderer sprite_StandingCG;
+    [SerializeField] private SpriteRenderer sprite_DialogBar;
+    [SerializeField] private Text txt_Dialogue;
+    [SerializeField] private Button button;
+
+    private bool isDialogue = false;
+
+    private int count = 0;
+
+    [SerializeField] private Dialogue[] dialogue;
+
+    public void ShowDialogue()
+    {
+        OnOff(true);
+
+        count = 0;
+        NextDialogue();
+        button.gameObject.SetActive(false);
+    }
+
+    private void NextDialogue()
+    {
+        txt_Dialogue.text = dialogue[count].dialogue;
+        sprite_StandingCG.sprite = dialogue[count].cg;
+        count++;
+    }
+
+    private void OnOff(bool _flag)
+    {
+        sprite_DialogBar.gameObject.SetActive(_flag);
+        sprite_StandingCG.gameObject.SetActive(_flag);
+        txt_Dialogue.gameObject.SetActive(_flag);
+
+        isDialogue = _flag;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isDialogue)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                if (count < dialogue.Length)
+                {
+                    NextDialogue();
+                }
+                else
+                {
+                    OnOff(false);
+                    button.gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
