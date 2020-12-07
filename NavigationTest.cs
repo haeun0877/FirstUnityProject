bbeb4b321bb71c6575889f13c2e2f22fb3e1e9b9
haeun0877 <70643208+haeun0877@users.noreply.github.com>
2020-12-07 19:38:21 +0900

    Create NavigationTest.cs

---
+++ NavigationTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationTest : MonoBehaviour
+{
+    Rigidbody myRigid;
+    [SerializeField] private float moveSpeed;
+
+    NavMeshAgent agent;
+
+    [SerializeField] private Transform tf_Destination;
+    private Vector3 originPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        originPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Patrol();
+    }
+
+    private void Patrol()
+    {
+        if(Vector3.Distance(transform.position, tf_Destination.position) < 0.1f)
+            agent.SetDestination(originPos);
+        else if(Vector3.Distance(transform.position, originPos) < 0.1f)
+            agent.SetDestination(tf_Destination.position);
+    }
+}
